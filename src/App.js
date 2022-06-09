ce74dcd0b6f4a49b220969ed9675b59e4bbe8bc3
jayramani8 <70213053+jayramani8@users.noreply.github.com>
2022-06-09 21:13:05 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer, createContext } from "react";
+import Dashboard from "./components/Dashboard";
+export const DemoContext = createContext("");
+const initialValue = {
+  keyValue: 0,
+  answer: "",
+};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "STORE_QUESTION":
+      let value = state.keyValue;
+      value = action.payload.keys;
+      return {
+        ...state,
+        keyValue: value,
+      };
+    case "0":
+      let flag = "";
+      for (let i = 1; i <= action.payload; i++) {
+        flag += i + ",";
+      }
+      return {
+        ...state,
+        answer: flag,
+      };
 
+      // eslint-disable-next-line no-unreachable
+      return {
+        ...state,
+        keyValue: value,
+      };
+    default:
+      return {};
+  }
+};
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialValue);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DemoContext.Provider value={{ state, dispatch }}>
+      <Dashboard />
+    </DemoContext.Provider>
   );
 }
 
---
+++ src/components/Answer.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useContext } from "react";
+import Result from "./Result";
+import { DemoContext } from "../App";
+import { listArray } from "./Question";
+const Answer = () => {
+  const [value, setValue] = useState(0);
+  const { state, dispatch } = useContext(DemoContext);
+  //console.log(listArray);
+  const AnswerHeading = listArray.at(state.keyValue);
+
+  const SubmitInput = (e) => {
+    e.preventDefault();
+    dispatch({ type: `${state.keyValue}`, payload: value });
+  };
+  return (
+    <div>
+      <h1>{AnswerHeading.question}</h1>
+      <form onSubmit={SubmitInput}>
+        <input
+          type="number"
+          min={1}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+        />
+        <button type="submit">Click</button>
+      </form>
+      <div className="h-50">answer</div>
+      <Result />
+    </div>
+  );
+};
+
+export default Answer;
